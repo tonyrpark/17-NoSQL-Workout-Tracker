83efeb7ed7405bb57eb55e3b1bff71be08443ad7
tonyrpark <tonyrpark@gmail.com>
2021-04-15 14:15:26 -0700

    finishing up api-routes.js and html-routes.js, checking server.js for functionality next

---
+++ routes/api-routes.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const db = require("../models/'); 
+
+// Routes
+
+router.get('/api/workouts', (req, res) => {
+    db.Workout.find({})
+    .then(dbWorkout => {
+        console.log(dbWorkout); 
+        res.json(dbWorkout); 
+    })
+    .catch(err=> {
+        res.status(400).json(err);
+    }); 
+}); 
+
+router.post('/api/workouts', ({ body }, res) => {
+    db.Workout.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout); 
+    })
+    .catch(err => {
+        res.status(400).json(err); 
+    }); 
+}); 
+
+router.put('/api/workouts/:id', (req, res) => {
+    db.Workout.updateOne(
+        { _id: req.params.id },
+        { $push: { exercises: req.body } }
+    )
+    .then(dbWorkout => {
+        res.json(dbWorkout); 
+    })
+    .catch(err => { 
+        res.json(err); 
+    }); 
+}); 
+// Get request for last 7 workouts
+router.get('/api/workouts/range', (req, res) => {
+    // Organizes workouts by most recent first
+    db.Workout.find({})
+      .sort({ _id: -1 })
+      .limit(7)
+      .then(dbWorkout => {
+        console.log(dbWorkout);
+        res.json(dbWorkout);
+        console.log(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  module.exports = router;
